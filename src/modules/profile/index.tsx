7b3bd1fdc9df78c28e38f9e497853b7a0a859324
Newton Duarte <newton_@hotmail.com>
2024-03-20 20:58:20 -0300

    refactor: profile page folder structure

--- src/modules/profile/index.tsx
+++ src/modules/profile/index.tsx
@@ -1,18 +1,21 @@
-import { BookCard } from '@/components/BookCard'
-import { User } from 'phosphor-react'
+import {
+  BookOpen,
+  BookmarkSimple,
+  Books,
+  MagnifyingGlass,
+  User,
+  UserList,
+} from 'phosphor-react'
 import { Text } from '@/components/Text'
-import { booksReviews } from '@/data/booksReviews'
+import { TextField } from '@/components/TextField'
 
-import * as S from '@/styles/pages/Profile.styles'
+import * as S from './styles'
+import { BookReview } from './BookReview'
+import { booksReviews } from '@/data/booksReviews'
 import Image from 'next/image'
-import { Rating } from '@/components/Rating'
-import { useRouter } from 'next/router'
-import { EXPLORE_ROUTE, PROFILE_ROUTE } from '@/constants/app-routes'
-import { ButtonLink } from '@/components/ButtonLink'
+import { FlexCol } from '@/components/FlexCol'
 
 export function Profile() {
-  const router = useRouter()
-
   return (
     <S.Container>
       <S.Header>
@@ -21,6 +24,78 @@ export function Profile() {
           Perfil
         </Text>
       </S.Header>
+
+      <S.Content>
+        <S.BooksReviewsList>
+          <S.SearchContainer>
+            <TextField
+              placeholder="Buscar livro avaliado"
+              endIcon={<MagnifyingGlass size={20} />}
+            />
+          </S.SearchContainer>
+
+          <BookReview book={booksReviews[2].book} />
+          <BookReview book={booksReviews[1].book} />
+        </S.BooksReviewsList>
+
+        <S.Analytics>
+          <Image
+            src="https://github.com/newton-duarte.png"
+            width={72}
+            height={72}
+            alt=""
+          />
+          <FlexCol>
+            <Text as="h3" size="lg">
+              Newton Duarte
+            </Text>
+            <Text as="span" size="sm">
+              membro desde 2024
+            </Text>
+          </FlexCol>
+
+          <S.Divider />
+
+          <S.AnalyticsItems>
+            <S.AnalyticsItem>
+              <BookOpen size={32} />
+              <FlexCol>
+                <Text as="h6">3853</Text>
+                <Text as="span" size="sm">
+                  Páginas lidas
+                </Text>
+              </FlexCol>
+            </S.AnalyticsItem>
+            <S.AnalyticsItem>
+              <Books size={32} />
+              <FlexCol>
+                <Text as="h6">10</Text>
+                <Text as="span" size="sm">
+                  Livros avaliados
+                </Text>
+              </FlexCol>
+            </S.AnalyticsItem>
+            <S.AnalyticsItem>
+              <UserList size={32} />
+              <FlexCol>
+                <Text as="h6">8</Text>
+                <Text as="span" size="sm">
+                  Autores lidos
+                </Text>
+              </FlexCol>
+            </S.AnalyticsItem>
+            <S.AnalyticsItem>
+              <BookmarkSimple size={32} />
+              <FlexCol>
+                <Text as="h6">Computação</Text>
+                <Text as="span" size="sm">
+                  Categoria mais lida
+                </Text>
+              </FlexCol>
+            </S.AnalyticsItem>
+          </S.AnalyticsItems>
+        </S.Analytics>
+      </S.Content>
     </S.Container>
   )
 }
---
+++ src/modules/profile/styles.ts
@@ -0,0 +1,96 @@
+import { styled } from '@/styles'
+
+export const Container = styled('div', {
+  marginTop: '4.5rem',
+})
+
+export const Header = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.75rem',
+  marginBottom: '$10',
+
+  svg: {
+    color: '$green100',
+  },
+
+  h2: {
+    fontWeight: 'bold',
+  },
+})
+
+export const Content = styled('div', {
+  marginTop: '2.5rem',
+  display: 'grid',
+  gridTemplateColumns: '1fr 20.25rem',
+  gap: '4rem',
+})
+
+export const BooksReviewsList = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.75rem',
+})
+
+export const Analytics = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  textAlign: 'center',
+  flexDirection: 'column',
+  alignSelf: 'flex-start',
+  borderLeft: '1px solid $gray700',
+  padding: '0 3.625rem $5',
+
+  img: {
+    border: '2px solid #7FD1CC',
+    marginBottom: '$5',
+    borderRadius: '$full',
+    objectFit: 'cover',
+  },
+
+  h3: {
+    color: '$gray100',
+  },
+
+  span: {
+    color: '$gray400',
+  },
+})
+
+export const SearchContainer = styled('div', {
+  marginBottom: '$8',
+})
+
+export const Divider = styled('div', {
+  width: '2rem',
+  height: '0.25rem',
+  background: '$gradient-horizontal',
+  borderRadius: '$full',
+  margin: '3.25rem 0',
+})
+
+export const AnalyticsItems = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$10',
+})
+
+export const AnalyticsItem = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: '2rem 1fr',
+  alignItems: 'center',
+  gap: '$5',
+  textAlign: 'left',
+
+  svg: {
+    color: '$green100',
+  },
+
+  h6: {
+    color: '$gray200',
+  },
+
+  span: {
+    color: '$gray300',
+  },
+})
