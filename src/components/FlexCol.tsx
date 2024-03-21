0baad5e9e52427975ff82cfa3223470a9cd035b7
Newton Duarte <newton_@hotmail.com>
2024-03-21 20:31:08 -0300

    feat: add component props to flex col

--- src/components/FlexCol.tsx
+++ src/components/FlexCol.tsx
@@ -1,6 +1,9 @@
+import { ComponentProps } from 'react'
 import { styled } from '@/styles'
 
 export const FlexCol = styled('div', {
   display: 'flex',
   flexDirection: 'column',
 })
+
+export interface FlexColProps extends ComponentProps<typeof FlexCol> {}
