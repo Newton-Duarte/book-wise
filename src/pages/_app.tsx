8051558b2b560bc9942c066af5488dc51640a8e5
Newton Duarte <newton_@hotmail.com>
2024-03-26 21:45:55 -0300

    feat: add session provider to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { globalStyles } from '@/styles/global'
 import { NextPage } from 'next'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { ReactElement, ReactNode } from 'react'
 
@@ -13,8 +14,15 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <SessionProvider session={session}>
+      {getLayout(<Component {...pageProps} />)}
+    </SessionProvider>
+  )
 }
