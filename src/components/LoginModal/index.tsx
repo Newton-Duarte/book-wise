491aca98c34a7db1ac04fe16c747d26a91d9592d
Newton Duarte <newton_@hotmail.com>
2024-04-01 17:45:13 -0300

    feat: implement login modal

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -1,14 +1,20 @@
 import Image from 'next/image'
-import * as Dialog from '@radix-ui/react-dialog'
-import * as S from './styles'
+import { signIn } from 'next-auth/react'
+import { forwardRef } from 'react'
 import { X } from 'phosphor-react'
 import { Text } from '../Text'
 import { FlexCol } from '../FlexCol'
 import googleLogoImg from '@/assets/images/google-logo.png'
 import githubLogoImg from '@/assets/images/github-logo.png'
-import { forwardRef } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+
+import * as S from './styles'
 
 export const LoginModal = forwardRef<HTMLDivElement>((props, ref) => {
+  const handleGoogleSignIn = async () => {
+    await signIn('google')
+  }
+
   return (
     <Dialog.Portal>
       <S.Overlay />
@@ -16,21 +22,19 @@ export const LoginModal = forwardRef<HTMLDivElement>((props, ref) => {
         <S.CloseButton>
           <X size={24} />
         </S.CloseButton>
-        <Dialog.Description>
-          <Text as="h2" style={{ textAlign: 'center', marginBottom: '2.5rem' }}>
-            Faça login para deixar sua avaliação
-          </Text>
-          <FlexCol style={{ gap: '1rem' }}>
-            <S.LoginButton>
-              <Image src={googleLogoImg} width={32} height={32} alt="" />
-              Entrar com Google
-            </S.LoginButton>
-            <S.LoginButton>
-              <Image src={githubLogoImg} width={32} height={32} alt="" />
-              Entrar com GitHub
-            </S.LoginButton>
-          </FlexCol>
-        </Dialog.Description>
+        <Text as="h2" style={{ textAlign: 'center', marginBottom: '2.5rem' }}>
+          Faça login para deixar sua avaliação
+        </Text>
+        <FlexCol style={{ gap: '1rem' }}>
+          <S.LoginButton onClick={handleGoogleSignIn}>
+            <Image src={googleLogoImg} width={32} height={32} alt="" />
+            Entrar com Google
+          </S.LoginButton>
+          <S.LoginButton>
+            <Image src={githubLogoImg} width={32} height={32} alt="" />
+            Entrar com GitHub
+          </S.LoginButton>
+        </FlexCol>
       </S.Content>
     </Dialog.Portal>
   )
