f16a50169eb60bc451a85adb0f57377f02f0e0e2
Newton Duarte <newton_@hotmail.com>
2024-04-03 21:19:43 -0300

    feat: implement rating component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,15 +1,88 @@
-import { Star, StarHalf } from 'phosphor-react'
+import { useMemo } from 'react'
+import { Star } from 'phosphor-react'
 
 import * as S from './styles'
 
-export function Rating() {
-  return (
-    <S.Container>
-      <Star weight="fill" size={16} />
-      <Star weight="fill" size={16} />
-      <Star weight="fill" size={16} />
-      <Star weight="fill" size={16} />
-      <StarHalf weight="fill" size={16} />
-    </S.Container>
-  )
+type RatingProps = {
+  rate: number
+  readonly?: boolean
+}
+
+export function Rating({ rate, readonly = false }: RatingProps) {
+  const renderRatings = useMemo(() => {
+    switch (rate) {
+      case 0:
+        return (
+          <S.Container>
+            <Star size={16} />
+            <Star size={16} />
+            <Star size={16} />
+            <Star size={16} />
+            <Star size={16} />
+          </S.Container>
+        )
+      case 1:
+        return (
+          <S.Container>
+            <Star weight="fill" size={16} />
+            <Star size={16} />
+            <Star size={16} />
+            <Star size={16} />
+            <Star size={16} />
+          </S.Container>
+        )
+      case 2:
+        return (
+          <S.Container>
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star size={16} />
+            <Star size={16} />
+            <Star size={16} />
+          </S.Container>
+        )
+      case 3:
+        return (
+          <S.Container>
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star size={16} />
+            <Star size={16} />
+          </S.Container>
+        )
+      case 4:
+        return (
+          <S.Container>
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star size={16} />
+          </S.Container>
+        )
+      case 5:
+        return (
+          <S.Container>
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+          </S.Container>
+        )
+      default:
+        return (
+          <S.Container>
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star weight="fill" size={16} />
+            <Star size={16} />
+          </S.Container>
+        )
+    }
+  }, [rate])
+
+  return renderRatings
 }
